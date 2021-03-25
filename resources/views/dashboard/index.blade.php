b575ed21c79bee78beed289c579080d00d165076
mirim127 <mirrim127@gmail.com>
2021-03-25 12:23:44 +0700

    menambahkan paid dan unpaid page

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -85,7 +85,7 @@
             <div class="w-1/2"></div>
             <div x-data="{ isOpen: false }" class="relative w-1/2 flex justify-end">
                 <button @click="isOpen = !isOpen" class="realtive z-10 p-2 hover:bg-gray-200 focus:bg-gray-200">
-                   Halo, {{ auth()->user()->role->rolename }}
+                   Halo, {{ auth()->user()->name }}
                 </button>
                 <button x-show="isOpen" @click="isOpen = false" class="h-full w-full fixed inset-0 cursor-default"></button>
                 <div x-show="isOpen" class="absolute w-32 bg-white rounded-lg shadow-lg py-2 mt-16">
